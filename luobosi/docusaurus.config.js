9ba6ff0e33457a983ef37c15b20927e14b3a62cb
jack ning <github@bytedesk.com>
2022-08-05 11:42:18 +0800

    update

--- luobosi/docusaurus.config.js
+++ luobosi/docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'pengjinning', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -70,6 +70,10 @@ const config = {
             to: '/blog',
             label: '博客',
             position: 'left'
+          },
+          {
+            type: 'localeDropdown',
+            position: 'right',
           },
           {
             href: 'https://github.com/pengjinning/docs',
